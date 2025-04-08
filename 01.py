5f68bcffdedab01f9f3c721afdf5583db4a346e3
Dlima15 <danilo.lima7456@gmail.com>
2025-04-07 23:46:28 -0300

    if em cadeado

--- 01.py
+++ 01.py
@@ -1,3 +1,6 @@
+#Uma universidade realizará uma competição acadêmica. Para esta competição, só serão Aceitos estudantes que sejam maiores de idade
+#crie um programa que receba o RM e a idade de um aluno, e exiba uma mensagem confirmando o cadastro apenas caso o estudante seja maior de idade, e uma mensagem de errp caso o estudande seja menor de idade
+
 rm = input("Qual a seu Rm?")
 idade = int(input("Qual sua idade"))
 
@@ -5,6 +8,6 @@ if idade >= 18:
     print(f"Seu cadastro foi realizado aluno {rm}")
     print("Prosseguiremos via email")
 else:
-    print("Você é menor de idade tente novamente qauando tiver 18")
+    print("ERRO! Você é menor de idade tente novamente qauando tiver 18")
 
 print("O programa foi Finalizado")    
--- 02.py
+++ 02.py
@@ -1,1 +1,28 @@
-#Uma universidade realizará uma competição acadêmica. Para esta competição, só serão Aceitos estudantes que sejam maiores de idade
+# Uma universidade realizará uma competição acadêmica. Para esta competição,
+# só serão aceitos estudantes que sejam maiores de idade.
+# Crie um programa que receba o RM e a idade de um aluno e exiba uma mensagem
+# confirmando o estudante ser maior. Caso um aluno seja menor, informe ter
+# autorização dos pais. O cadastro deve ser realizado e o email deve ser
+# enviado para os responsáveis.
+# Caso contrário, o cadastro não será feito.
+
+rm = input("Qual a seu Rm?")
+idade = int(input("Qual sua idade?"))
+
+if idade >= 18:
+    print(f"Seu cadastro foi realizado aluno {rm}")
+    print("Prosseguiremos via email")
+else:
+    print("ERRO! Você tem autorização?")
+    A = input("Seus pais autorizaram, S - sim ou N - não?")
+    if A == "S":
+        print("OK, prosseguiremos por email")
+    else:
+        print("infelizmente não será possivel continuar, tente a autorização ou espere ficar maior de idade!")
+
+
+print("...")
+print("...")
+
+
+print("O Programa foi finalizado")
