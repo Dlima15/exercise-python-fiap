616d67045ffeb2c74beca01bbf73a43fe030ea6f
Dlima15 <danilo.lima7456@gmail.com>
2025-04-28 23:04:15 -0300

    métodos uteis

---
+++ 15 métodos úteis.py
@@ -0,0 +1,16 @@
+# Lista de números
+numeros = [5, 3, 8, 3, 9, 1]
+
+# Ordenar em ordem crescente
+numeros.sort()
+print(numeros)
+
+# Inverter a lista decrescente
+numeros.reverse()
+print(numeros)
+
+# Contar quantas vezes o número 3 aparece
+print("O número 3 aparece", numeros.count(3), "vezes.")
+
+# Encontrar a posição do número 9
+print("O número 9 está na posição", numeros.index(9))
