164e6846807621a3a4d19de606b6d2f785309f29
Dlima15 <danilo.lima7456@gmail.com>
2025-04-28 23:00:57 -0300

    12 & 13 criação e add

---
+++ 12 Listas criação e iteração.py
@@ -0,0 +1,6 @@
+# Criando uma lista de frutas
+frutas = ["Maçã", "Banana", "Morango"]
+
+# Iterando (percorrendo) a lista
+for fruta in frutas:
+    print(f"Eu gosto de {fruta}")
---
+++ 13 Inserção de dado.py
@@ -0,0 +1,8 @@
+# Lista vazia
+cidades = []
+
+# Adicionando cidades
+cidades.append("São Paulo")   # Adiciona no final
+cidades.insert(0, "Rio de Janeiro")  # Adiciona na posição 0
+
+print(cidades)
