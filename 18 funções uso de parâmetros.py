fb10194547ed5d443a791ae56cae6fda9708a938
Dlima15 <danilo.lima7456@gmail.com>
2025-04-28 23:10:48 -0300

    uso de paramêtros

---
+++ 18 funções uso de parâmetros.py
@@ -0,0 +1,7 @@
+# Função que recebe o nome da pessoa
+def cumprimentar(nome):
+    print(f"Olá, {nome}! Seja bem-vindo!")
+
+# Chamando a função com diferentes nomes
+cumprimentar("Lucas")
+cumprimentar("Maria")
