497373572e33744762a2ac7701fe07edd5f5ae02
Dlima15 <danilo.lima7456@gmail.com>
2025-04-28 23:08:34 -0300

    funcções def

---
+++ 17 funções.py
@@ -0,0 +1,12 @@
+# Funções ajudam a organizar o código e evitar repetições
+
+def saudacao():
+    print("Olá! Bem-vindo ao programa.")
+
+def SP():
+    print("São Paulo tricampeão do mundo")
+
+# Chamando a função
+saudacao()
+
+SP()
