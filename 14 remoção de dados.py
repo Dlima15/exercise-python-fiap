d9f73f09af87f63a785b6c2e826d6e3163c342fa
Dlima15 <danilo.lima7456@gmail.com>
2025-04-28 23:02:34 -0300

    remove

---
+++ 14 remoção de dados.py
@@ -0,0 +1,16 @@
+# Lista de animais
+animais = ["Cachorro", "Gato", "Peixe", "Papagaio"]
+
+# Remover pelo valor
+animais.remove("Peixe")
+
+# Remover pelo índice
+animais.pop(0)  # Remove o primeiro
+
+# Deletar elemento específico
+del animais[1]  # Remove o segundo (atual)
+
+# Limpar a lista
+animais.clear()
+
+print(animais)  # Lista agora está vazia
